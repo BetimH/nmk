13b3ad431d7b914cce5f0190ed7e73a805f8f570
web <103862044+BetimH@users.noreply.github.com>
2025-06-26 06:36:38 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const hamMenu = document.querySelector('.ham-menu');
+const menu = document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener('click', () => {
+hamMenu.classList.toggle('active');
+menu.classList.toggle('active');
+})
